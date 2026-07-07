a5d1b7b80e275c72d57f6e2a585ffe721f6d2813
Luuk Blankenstijn <git@luukblankenstijn.nl>
2026-07-07 18:13:05 +0200

    pcgewisinfo: simplify to daily poweroff at 23:00 (BIOS handles wake)

--- hosts/pcgewisinfo/schedule.nix
+++ hosts/pcgewisinfo/schedule.nix
@@ -1,30 +1,18 @@
-{ pkgs, ... }:
-
-let
-  # Arm RTC wake for the next 08:00 local time, then power off.
-  # RTC wake from S5 is firmware-dependent; if the box does not power on by
-  # itself, enable "Wake on RTC"/"Auto Power On" in BIOS as the reliable path.
-  poweroff = pkgs.writeShellScript "daily-poweroff" ''
-    set -eu
-    ${pkgs.util-linux}/bin/rtcwake -m no -t "$(${pkgs.coreutils}/bin/date -d 'tomorrow 08:00' +%s)"
-    ${pkgs.systemd}/bin/systemctl poweroff
-  '';
-in
-{
-  systemd.services.daily-poweroff = {
-    description = "Arm RTC wake for 08:00 and power off";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = poweroff;
+_: {
+  systemd.timers.daily-poweroff = {
+    description = "Power off daily at 23:00";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 23:00:00";
+      Persistent = false;
     };
   };
 
-  systemd.timers.daily-poweroff = {
-    description = "Power off daily at 22:00";
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnCalendar = "*-*-* 22:00:00";
-      Persistent = false;
+  systemd.services.daily-poweroff = {
+    description = "Power off the machine";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "/run/current-system/sw/bin/systemctl poweroff";
     };
   };
 }
